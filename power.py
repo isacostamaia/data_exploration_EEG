ffcc0c8fca7a0ec2ce0a71ad1425fd7589070eac
Isabella Costa Maia <costamai@desktop-423.gipsa-lab.grenoble-inp.fr>
2024-09-24 17:06:36 +0200

    frms

--- power.py
+++ power.py
@@ -1,13 +1,15 @@
+import numpy as np
+
 def field_root_mean_square(class_epochs):
     
     class_epochs_data = class_epochs.get_data() #array of shape (n_epochs, n_channels, n_times)
-    Xk = np.reshape(class_epochs_data, (class_epochs_data.shape[1], class_epochs_data.shape[0]*class_epochs_data.shape[2])) #Xk in R^(NxT)
 
-    n = Xk.shape[0]
+    n = class_epochs_data.shape[1] #n_channels
     centering_matrix = np.eye(n) - np.ones((n, n)) / n
     #centered_Xk has each column with zero mean
-    centered_Xk = centering_matrix@Xk
+    centered_Xks = [centering_matrix@Xk for Xk in class_epochs_data]
 
-    #TODO:
-    #phi is the std of the columns of centered_Xk
-    pass
+    #frms_i is the std of the columns of centered_Xk
+    frms = np.stack([Xk.std(axis=0) for Xk in centered_Xks])  #each pfmrs_i has dim = n_times, frms will be n_epochs x n_times
+
+    return frms
