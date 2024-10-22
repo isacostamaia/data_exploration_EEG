d263f831439096a08eb42faeeed84237b4df9f88
Isabella Costa Maia <costamai@desktop-423.gipsa-lab.grenoble-inp.fr>
2024-10-22 17:14:18 +0200

    temporal filter

--- data_processing.py
+++ data_processing.py
@@ -26,9 +26,9 @@ def get_clean_epochs(dataset, subjects_list=[1], paradigm = P300(), reject_value
      
     return epochs
     
-class SpatialFilter:
+class Filter:
     """
-    Spatial filter based on epochs object and a given class_ 
+    Spatial or Temporal filter based on epochs object and a given class_ 
 
 
     attributes: 
@@ -36,37 +36,69 @@ class SpatialFilter:
             p: number of spatial filter components
             A_p: n_channels x p dimensional filter matrix
             B_p: p x n_channels dimensional filter matrix
+            spatial: True if spatial filter, False if temporal
     """
-    def __init__(self, epochs, p = 4):
-        
+    def __init__(self, epochs, p = 4, spatial = True):
+
         self.p = p
         self.epochs = copy.deepcopy(epochs)
-        
-    def fit(self,class_ = "Target"):
+        self.spatial = spatial
+
+
+    def fit(self, class_ = "Target"):
         """
-        Fit spatial filter in class_ of epochs object
+        Fit spatial or temporal filter in class_ of epochs object
             class_: class to which filter is fitted
     
-        """
+        """        
+
         epochs_data = self.epochs.get_data()*1e6 #n_epochs x n_channels x n_times using epochs from all classes
-        covs = np.stack([e_i@e_i.T/e_i.shape[-1] for e_i in epochs_data]) #n_epochs x n_channels x n_channels
-        Cs = np.mean(covs, axis=0) #n_channels x n_channels (1.12)
-        
         X_bar_class = self.epochs[class_].average().get_data()*1e6
-        C_bar_class = (1/X_bar_class.shape[-1])* (X_bar_class)@(X_bar_class.T) #n_channels x n_channels
-    
+        
+        if not self.spatial:
+            e,c,t = epochs_data.shape
+            epochs_data = epochs_data.reshape(e,t,c)
+            X_bar_class = X_bar_class.reshape(t,c)
+        
+        covs = np.stack([e_i@e_i.T/e_i.shape[-1] for e_i in epochs_data]) #n_epochs x n_channels x n_channels if spatial
+        
+        Cs = np.mean(covs, axis=0) #n_channels x n_channels (1.12) if spatial
+        
+        C_bar_class = (1/X_bar_class.shape[-1])* (X_bar_class)@(X_bar_class.T) #n_channels x n_channels if spatial (1.11)
         eigen_vals, B = eigh(C_bar_class, Cs)
-    
+        
         A = pinv(B)
         self.A_p = A[-self.p:,:].T
         self.B_p = B[:,-self.p:].T
+    
+    # def fit(self,class_ = "Target"):
+    #     """
+    #     Fit spatial filter in class_ of epochs object
+    #         class_: class to which filter is fitted
+    
+    #     """
+    #     epochs_data = self.epochs.get_data()*1e6 #n_epochs x n_channels x n_times using epochs from all classes
+    #     covs = np.stack([e_i@e_i.T/e_i.shape[-1] for e_i in epochs_data]) #n_epochs x n_channels x n_channels
+    #     Cs = np.mean(covs, axis=0) #n_channels x n_channels (1.12)
+        
+    #     X_bar_class = self.epochs[class_].average().get_data()*1e6
+    #     C_bar_class = (1/X_bar_class.shape[-1])* (X_bar_class)@(X_bar_class.T) #n_channels x n_channels
+    
+    #     eigen_vals, B = eigh(C_bar_class, Cs)
+    
+    #     A = pinv(B)
+    #     self.A_p = A[-self.p:,:].T
+    #     self.B_p = B[:,-self.p:].T
         
 
     def apply(self,epochs):
         """
         Returns filtered epochs (not inplace)
         """
-        epoch_denoiser = lambda epoch: (self.A_p)@(self.B_p)@epoch
+        if self.spatial:
+            epoch_denoiser = lambda epoch: (self.A_p)@(self.B_p)@epoch
+        else: #temporal filter
+            epoch_denoiser = lambda epoch: epoch@(self.A_p)@(self.B_p)
         epochs = copy.deepcopy(epochs)
         epochs.apply_function(epoch_denoiser, picks='all', channel_wise=False)
         return epochs
